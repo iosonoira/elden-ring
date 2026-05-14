cab31da2a377d2ce16c2dcd38324585a7b12701b
iosonoira <matteo.dessi93@gmail.com>
2026-05-14 17:40:34 +0200

    feat(wiki): add WikiDropList component

---
+++ app/components/wiki/WikiDropList.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+defineProps<{ drops: string[] }>()
+</script>
+
+<template>
+  <ul class="wiki-drop-list">
+    <li v-for="drop in drops" :key="drop" class="wiki-drop-list__item">
+      <Icon name="material-symbols:diamond-outline" size="14" class="wiki-drop-list__icon" />
+      <span>{{ drop }}</span>
+    </li>
+  </ul>
+</template>
+
+<style scoped lang="scss">
+.wiki-drop-list {
+  list-style: none;
+  padding: 0;
+  margin: 0;
+  display: flex;
+  flex-direction: column;
+  gap: $space-2;
+
+  &__item {
+    display: flex;
+    align-items: center;
+    gap: $space-3;
+    font-family: $font-body;
+    font-size: $text-body;
+    color: $color-on-surface;
+    padding: $space-2 0;
+  }
+
+  &__icon {
+    color: $color-primary;
+    flex-shrink: 0;
+  }
+}
+</style>
